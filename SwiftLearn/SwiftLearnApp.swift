a72816706abad65fcb89c9ca29671704bf2d2ddb
icalajah <ahmad.aziz@wearesocialights.com>
2022-11-06 20:31:47 +0700

    Initial Commit

---
+++ SwiftLearn/SwiftLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLearnApp.swift
+//  SwiftLearn
+//
+//  Created by Ahmad Aziz on 06/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
